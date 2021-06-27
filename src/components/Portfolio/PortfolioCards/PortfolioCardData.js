f910775145a765d76d1e2a1d58547205879f7e99
Karsenh <KarsenHansen@gmail.com>
2021-06-27 13:45:21 -0700

    added url links for github portofolio buttons

--- src/components/Portfolio/PortfolioCards/PortfolioCardData.js
+++ src/components/Portfolio/PortfolioCards/PortfolioCardData.js
@@ -4,34 +4,34 @@ export const Projects = [
     platform: 'MERN Web App',
     description:
       'Collaborated with a team of three to develop a full-stack web application for Boeing capable of streaming 360-degree 4K video in a navigable format witih location services for dynamic POIs.',
-    link: 'https://www.google.com',
+    link: 'https://github.com/Full-Stack-Hyperbolic/BOEING-360',
   },
   {
     title: 'Senior Research Project',
     platform: 'MERN Web App',
     description:
       'Developed a full-stack user-authentication and profile system integrating several APIs such as GitHub using MongoDB, Express, React, and Node.js - deployed to remote Heroku server.',
-    link: 'https://www.google.com',
+    link: 'https://github.com/Karsenh/MERN-User-Profile-Project',
   },
   {
     title: 'Memories',
     platform: 'MERN Web App',
     description:
       'Currently developing a full-stack web application encapsulating the fundamentals of large-scale platforms including user-authentication and security to basic CRUD operations.',
-    link: 'https://www.google.com',
+    link: 'https://github.com/Full-Stack-Hyperbolic/Fundamental-User-App',
   },
   {
     title: 'Web-Socket Messaging',
     platform: 'React.js',
     description:
       'Developed basic web-based messaging app similar to Whatsapp using React.js and web-socket.io.',
-    link: 'https://www.google.com',
+    link: 'https://github.com/Karsenh/React-Messaging-App',
   },
   {
     title: 'AR Memory Game',
     platform: 'iOS',
     description:
       'Developed an augmented reality flip-card memory game with 3D assets for an iOS course at GU.',
-    link: 'https://www.google.com',
+    link: 'https://github.com/iOS-Hyperbolic/iOS-AR-Memory-App',
   },
 ];
