e5e3937ba488b3209e6cf0e18c28f56db2f7f782
Karsenh <KarsenHansen@gmail.com>
2021-07-12 03:03:03 -0700

    updated portfolio data

--- src/components/Portfolio/PortfolioCards/PortfolioCardData.js
+++ src/components/Portfolio/PortfolioCards/PortfolioCardData.js
@@ -8,14 +8,6 @@ export const Projects = [
     demoLink: 'https://aqueous-escarpment-60789.herokuapp.com/',
   },
   {
-    title: 'Senior Research Project',
-    platform: 'MERN Web App',
-    description:
-      'Developed a full-stack user-authentication and profile system integrating several APIs such as GitHub using MongoDB, Express, React, and Node.js - deployed to remote Heroku server.',
-    link: 'https://github.com/Karsenh/MERN-User-Profile-Project',
-    demoLink: 'https://mern-310-final.herokuapp.com/',
-  },
-  {
     title: 'To-Do Notes',
     platform: 'React.js',
     description:
@@ -31,18 +23,32 @@ export const Projects = [
     link: 'https://github.com/Full-Stack-Hyperbolic/BOEING-360',
   },
   {
+    title: 'Senior Research Project',
+    platform: 'MERN Web App',
+    description:
+      'Developed a full-stack user-authentication and profile system integrating several APIs such as GitHub using MongoDB, Express, React, and Node.js - deployed to remote Heroku server.',
+    link: 'https://github.com/Karsenh/MERN-User-Profile-Project',
+  },
+  {
     title: 'Memories',
     platform: 'MERN Web App',
     description:
-      'Currently developing a full-stack web application encapsulating the fundamentals of large-scale platforms including user-authentication and security to basic CRUD operations.',
+      'Currently developing a full-stack web application encapsulating the fundamentals of large-scale platforms including user-authentication, security and CRUD operations.',
     link: 'https://github.com/Full-Stack-Hyperbolic/Fundamental-User-App',
   },
   {
     title: 'Web-Socket Messaging',
     platform: 'React.js',
     description:
-      'Developed basic web-based messaging app similar to Whatsapp using React.js and web-socket.io.',
+      'Developed basic web-based messaging application similar to Whatsapp using React.js and web-socket.io for real-time messaging.',
     link: 'https://github.com/Karsenh/React-Messaging-App',
+  },
+  {
+    title: 'Wildfire-Tracker',
+    platform: 'React.js',
+    description:
+      'React.js based web application for tracking wild-fires using NASAs open API.',
+    link: 'https://github.com/ReactJs-Hyperbolic/Wildfire-Tracker',
   },
   {
     title: 'AR Memory Game',
