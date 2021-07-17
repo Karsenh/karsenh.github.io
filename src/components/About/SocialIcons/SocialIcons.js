b0086194a1679c053b95353af5675d491781ba99
Karsenh <KarsenHansen@gmail.com>
2021-07-16 19:18:54 -0700

    increased the size of social icons in about me section

--- src/components/About/SocialIcons/SocialIcons.js
+++ src/components/About/SocialIcons/SocialIcons.js
@@ -5,16 +5,16 @@ const SocialIcons = () => {
   return (
     <div className='about-top-left-social-icons'>
       <a className='github-icon' href='https://github.com/Karsenh'>
-        <FaGithub size={40} />
+        <FaGithub size={45} />
       </a>
       <a
         className='instagram-icon'
         href='https://www.instagram.com/realkarsen/'
       >
-        <FaInstagram size={40} />
+        <FaInstagram size={45} />
       </a>
       <a className='linkedin-icon' href='https://www.linkedin.com/in/karsenh/'>
-        <FaLinkedin size={40} />
+        <FaLinkedin size={45} />
       </a>
     </div>
   );
