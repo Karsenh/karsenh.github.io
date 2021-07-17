be619c94da082ebea2bc6d28e9de9c47490381bf
Karsenh <KarsenHansen@gmail.com>
2021-07-16 19:17:04 -0700

    Added Typewriter effect to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import AnimatedWaves from './Waves/AnimatedWaves';
 import Particles from 'react-particles-js';
 import { Button } from '@material-ui/core';
 import DescriptionIcon from '@material-ui/icons/Description';
+import Typewriter from 'typewriter-effect';
 import myPDF from './myResume.pdf';
 
 const Header = () => {
@@ -12,6 +13,38 @@ const Header = () => {
         <h1>
           Hi, my name's <span style={{ fontWeight: '600' }}>Karsen</span>!
         </h1>
+
+        <div
+          style={{
+            fontFamily: 'inherit',
+            fontSize: '1.5rem',
+            display: 'flex',
+            margin: '2rem auto',
+            width: '50%',
+            justifyContent: 'center',
+          }}
+        >
+          <span>{'< '}</span>
+          <div style={{ fontStyle: 'italic' }}>
+            <Typewriter
+              options={{
+                strings: [
+                  'Full-Stack Web Dev',
+                  'Traveler',
+                  'Movie connoisseur',
+                  'Meme lord',
+                ],
+                autoStart: true,
+                loop: true,
+                delay: '100',
+                deleteSpeed: '75',
+              }}
+            />
+          </div>
+
+          <span>{' />'}</span>
+        </div>
+
         <Button
           onClick={() => (window.location.href = myPDF)}
           className='resume-btn'
