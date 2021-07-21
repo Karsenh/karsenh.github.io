4c8cf9e7b3808aaff43eb74ef132350f1d11346b
Karsenh <KarsenHansen@gmail.com>
2021-07-20 20:01:19 -0700

    Updated portfolio section with Memories project demo link

--- src/components/Portfolio/PortfolioCards/PortfolioCardData.js
+++ src/components/Portfolio/PortfolioCards/PortfolioCardData.js
@@ -1,15 +1,23 @@
 export const Projects = [
   {
     title: 'Sorting Visualizer',
-    platform: 'React.js',
+    platform: '(React.js)',
     description:
       'React.js based web-app for visualizing the merge-sort algorithm.',
     link: 'https://github.com/Karsenh/SortingVisualizer',
     demoLink: 'https://aqueous-escarpment-60789.herokuapp.com/',
   },
   {
+    title: 'Memories',
+    platform: '(MERN)',
+    description:
+      'Full-stack (MERN) web app - encapsulating the fundamentals of Redux state management, RESTful APIs, user authentication, web sockets, and more.',
+    link: 'https://github.com/Full-Stack-Hyperbolic/Fundamental-User-App',
+    demoLink: 'https://klh-memories.netlify.app/',
+  },
+  {
     title: 'To-Do Notes',
-    platform: 'React.js',
+    platform: '(React.js)',
     description:
       'Obligatory To-Do style web app made using React.js framework and non-persistent state for to-dos.',
     link: 'https://github.com/Karsenh/React-Messaging-App',
@@ -17,42 +25,35 @@ export const Projects = [
   },
   {
     title: 'Boeing-360',
-    platform: 'MERN Web App',
+    platform: '(MERN)',
     description:
       'Collaborated with a team of three to develop a full-stack web application for Boeing capable of streaming 360-degree 4K video in a navigable format with location services for dynamic POIs. Patent pending.',
     link: 'https://github.com/Full-Stack-Hyperbolic/BOEING-360',
   },
   {
     title: 'Senior Research Project',
-    platform: 'MERN Web App',
+    platform: '(MERN)',
     description:
       'Developed a full-stack user-authentication and profile system integrating several APIs such as GitHub using MongoDB, Express, React, and Node.js - deployed to remote Heroku server.',
     link: 'https://github.com/Karsenh/MERN-User-Profile-Project',
   },
   {
-    title: 'Memories',
-    platform: 'MERN Web App',
-    description:
-      'Currently developing a full-stack web application encapsulating the fundamentals of large-scale platforms including user-authentication, security and CRUD operations.',
-    link: 'https://github.com/Full-Stack-Hyperbolic/Fundamental-User-App',
-  },
-  {
     title: 'Web-Socket Messaging',
-    platform: 'React.js',
+    platform: '(React.js)',
     description:
       'Developed basic web-based messaging application similar to Whatsapp using React.js and web-socket.io for real-time messaging.',
     link: 'https://github.com/Karsenh/React-Messaging-App',
   },
   {
     title: 'Wildfire-Tracker',
-    platform: 'React.js',
+    platform: '(React.js)',
     description:
       'React.js based web application for tracking wild-fires using NASAs open API.',
     link: 'https://github.com/ReactJs-Hyperbolic/Wildfire-Tracker',
   },
   {
     title: 'AR Memory Game',
-    platform: 'iOS',
+    platform: '(iOS)',
     description:
       'Developed an augmented reality flip-card memory game with 3D assets for an iOS course at GU.',
     link: 'https://github.com/iOS-Hyperbolic/iOS-AR-Memory-App',
